498d3aac288bb5d4b94d8392e194e75ec553ad0c
NEWE69 <newe00@protonmail.com>
2025-09-03 10:41:09 +0200

    feat: treemacs bind + delete latex auto-compile

--- doom/config.el
+++ doom/config.el
@@ -1,33 +1,29 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-
-;; Load latex config 
+;; LaTeX settings
 (setq +latex-viewers '(zathura))
 
-;; Use XeLaTeX as the default LaTeX compiler
 (after! latex
-  ;; Tell AUCTeX to use xelatex by default
+  ;; XeLaTeX as default
   (setq TeX-engine 'xetex
-        TeX-command-default "XeLaTeX")
+        TeX-PDF-mode t
+        TeX-command-default "XeLaTeX"
+        TeX-save-query nil      ;; auto-save buffers before compiling
+        TeX-show-compilation t) ;; show compilation buffer
 
-  ;; Add xelatex to the command list if not already there
+  ;; Add XeLaTeX to command list
   (add-to-list 'TeX-command-list
                '("XeLaTeX"
-                 "xelatex -interaction=nonstopmode %s"
+                 "xelatex -interaction=nonstopmode -shell-escape %s"
                  TeX-run-TeX nil t
                  :help "Run XeLaTeX")))
 
-;; Auto-compile LaTeX on save
-(add-hook 'LaTeX-mode-hook
-          (lambda ()
-            (add-hook 'after-save-hook #'TeX-command-master nil t)))
+;;Bindings
+
+;; Select the treemacs window
+(map! :leader
+      :desc "Focus Treemacs"
+      "p t" #'treemacs-select-window)
